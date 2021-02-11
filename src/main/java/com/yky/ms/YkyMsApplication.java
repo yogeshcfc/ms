343effb1380dc043dc4e507a09c6fea917a01223
yogeshcfc <63028527+yogeshcfc@users.noreply.github.com>
2021-02-10 20:03:34 -0700

    initial commit

---
+++ src/main/java/com/yky/ms/YkyMsApplication.java
@@ -0,0 +1,13 @@
+package com.yky.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YkyMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yky/ms/YkyMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yky.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YkyMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
